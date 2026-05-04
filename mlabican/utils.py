ec891ac0815889d41c49a14d5128daf0ded20673
ArthurGorgonio <gorgonioarthur@gmail.com>
2026-05-04 16:07:31 -0300

    feat(ssl methods): move utils to new structure
    Select the labeled instances using stratification to keep the ratio between class balance;
    Remove old classifier list configuration.
    
    Issue: #27

--- mlabican/utils.py
+++ mlabican/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def has_method(obj: object, method_name: str):
     """
     Check if an object has a callable method with the given name.
@@ -13,3 +16,39 @@ def has_method(obj: object, method_name: str):
     attribute = getattr(obj, method_name, None)
 
     return callable(attribute)
+
+
+def select_labels(
+    instances: np.ndarray, labels: np.ndarray, label_percentage: float
+) -> np.ndarray:
+    """
+    Selects the instances that will be left without a label randomly
+    based on the distribution of classes (stratified selection) and the
+    percentage of labeled instances. The minimum number of labeled
+    instances per class is 1 instance to ensure the representation of
+    minority classes.
+
+    Args:
+        instances (np.ndarray): Instances
+        labels (np.ndarray): Labels
+        labelled_percentage (float): % of instances that will have a
+            label.
+
+    Returns:
+        np.ndarray: the array of labels already mapped to the labeled
+        and unlabeled instances.
+    """
+    class_dist = np.bincount(labels)
+    min_acceptable = np.trunc(class_dist * label_percentage)
+    selected = []
+
+    for lab, cls_dist in enumerate(min_acceptable):
+        selected += np.random.choice(
+            np.where(labels == lab)[0], int(cls_dist) or 1, replace=False
+        ).tolist()
+
+    mask = np.ones(len(instances), bool)
+    mask[selected] = 0
+    labels[mask] = -1
+
+    return labels
